ac2dd3e454ba757cfa2c9d907a421ebda05a87e2
rohandamani <damanirohan@gmail.com>
2017-02-20 16:16:25 -0800

    add courts route

---
+++ routes/courts.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+
+var caseCategoryDummyOptions = {'': ''}
+caseCategoryDummyOptions["Class Action - Under $10,000"] = "Class Action - Under $10,000"
+caseCategoryDummyOptions["Class Action - Unlimited"] = "Class Action - Unlimited"
+caseCategoryDummyOptions["Collections (Other) - Between $10,000 and $25,000"] = "Collections (Other) - Between $10,000 and $25,000"
+
+var caseTypeDummyOptions = {'': ''}
+caseTypeDummyOptions["Civil"] = "Civil"
+
+var courthouseDummyOptions = {'': ''}
+courthouseDummyOptions["Alhambra Courthouse"] = "Alhambra Courthouse"
+courthouseDummyOptions["Burbank Courthouse"] = "Burbank Courthouse"
+courthouseDummyOptions["Central Civil West"] = "Central Civil West"
+courthouseDummyOptions["Chatsworth Courthouse"] = "Chatsworth Courthouse"
+courthouseDummyOptions["Downey Courthouse"] = "Downey Courthouse"
+courthouseDummyOptions["Governer Courthouse"] = "Governer Courthouse"
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(courthouseDummyOptions);
+});
+
+module.exports = router;
