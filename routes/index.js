a576c3f6e87bd25f3bf5930f5c9023aaebba031e
rohandamani <damanirohan@gmail.com>
2017-03-06 18:45:51 -0800

    add fake data to users

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Fake API', routes: '/efsp' });
+  res.render('index', { title: 'My Fake API', routes: '/efsp /users' });
 });
 
 module.exports = router;
