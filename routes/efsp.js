0828defa7cc99f6277531cb074b4b7b6dc415bd1
rohandamani <damanirohan@gmail.com>
2017-07-07 17:32:54 -0700

    edit efsp data

--- routes/efsp.js
+++ routes/efsp.js
@@ -9,7 +9,7 @@ caseCategoryDummyOptions["Collections (Other) - Between $10,000 and $25,000"] = 
 var caseTypeDummyOptions = {"Civil": "Civil"}
 caseTypeDummyOptions["Other"] = "Other"
 
-var courthouseDummyOptions = [{name: "Alhambra Courthouse"},{name:"Burbank Courthouse"},{name:"Central Civil West"},{name:"Chatsworth Courthouse"},{name:"Downey Courthouse"},{name:"Governer Courthouse"}]
+var courthouseDummyOptions = [{name: "Alhambra Courthouse"},{name:"Burbank Courthouse"},{name:"Central Civil West"},{name:"Chatsworth Courthouse"},{name:"Downey Courthouse"},{name:"Governer Courthouse"},{name:"Capital Courthouse"}]
 
 router.get('/case/courthouse', function(req, res, next) {
   res.send(courthouseDummyOptions);
